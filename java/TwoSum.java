d0d09a587a24ae6d4426a330ba2f401bd0dbcca6
DerekDuan <duan.shiqi@gmail.com>
2023-10-14 23:35:42 +0800

    Add java two_sum

---
+++ java/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            Integer index = map.get(complement);
+            if (index != null) {
+                return new int[] {index, i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
